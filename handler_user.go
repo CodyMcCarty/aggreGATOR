7368ca8415225f78ac12eba51868b3a16516a9b3
Cody McCarty <codyjmccarty@gmail.com>
2025-08-28 16:04:07 -0500

    reafactor to course code

--- handler_user.go
+++ handler_user.go
@@ -24,16 +24,6 @@ func handlerListUsers(s *state, cmd command) error {
 	return nil
 }
 
-func handlerReset(s *state, cmd command) error {
-	err := s.db.DeleteUsers(context.Background())
-	if err != nil {
-		return fmt.Errorf("couldn't delete users: %w", err)
-	}
-
-	fmt.Println("Database reset successfully!")
-	return nil
-}
-
 func handlerRegister(s *state, cmd command) error {
 	if len(cmd.Args) != 1 {
 		return fmt.Errorf("usage: %v <name>", cmd.Name)
