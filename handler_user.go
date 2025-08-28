181fd2ea208b34982414c872602d4971d282b65c
Cody McCarty <codyjmccarty@gmail.com>
2025-08-28 16:02:43 -0500

    c2.l5 List Users. Did refactor to course code again.

--- handler_user.go
+++ handler_user.go
@@ -9,6 +9,21 @@ import (
 	"github.com/google/uuid"
 )
 
+func handlerListUsers(s *state, cmd command) error {
+	users, err := s.db.GetUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't list users: %w", err)
+	}
+	for _, user := range users {
+		if user.Name == s.cfg.CurrentUserName {
+			fmt.Printf("* %v (current)\n", user.Name)
+			continue
+		}
+		fmt.Printf("* %v\n", user.Name)
+	}
+	return nil
+}
+
 func handlerReset(s *state, cmd command) error {
 	err := s.db.DeleteUsers(context.Background())
 	if err != nil {
