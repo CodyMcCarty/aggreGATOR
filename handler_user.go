cdb4540c00bea8bb1445f14d3fb5902bc448c8f2
Cody McCarty <codyjmccarty@gmail.com>
2025-08-28 15:49:27 -0500

    c2.l4 Reset. Not updating my code to the course

--- handler_user.go
+++ handler_user.go
@@ -9,6 +9,16 @@ import (
 	"github.com/google/uuid"
 )
 
+func handlerReset(s *state, cmd command) error {
+	err := s.db.DeleteUsers(context.Background())
+	if err != nil {
+		return fmt.Errorf("couldn't delete users: %w", err)
+	}
+
+	fmt.Println("Database reset successfully!")
+	return nil
+}
+
 func handlerRegister(s *state, cmd command) error {
 	if len(cmd.Args) != 1 {
 		return fmt.Errorf("usage: %v <name>", cmd.Name)
